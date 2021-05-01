0449918fc934570f9dd434dfe68539fd4b11f3d5
neelbavarva <neelbavarva5@gmail.com>
2021-04-30 23:43:35 -0400

    initial firebase setup with react

---
+++ react-app/src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDeKEaPG5jkINdX6aA2cALcbNWVpzA44gg",
+    authDomain: "react-crud-a622e.firebaseapp.com",
+    databaseURL: "https://react-crud-a622e-default-rtdb.firebaseio.com",
+    projectId: "react-crud-a622e",
+    storageBucket: "react-crud-a622e.appspot.com",
+    messagingSenderId: "437128248362",
+    appId: "1:437128248362:web:0ba932a422076a21939753"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
